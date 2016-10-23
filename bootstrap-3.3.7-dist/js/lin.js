1be34bdfeaca7bc6b751bd937569f8f671a6a4ae
Lochoa11 <linochoa11690@gmail.com>
2016-10-23 14:53:31 -0400

    working on keeping track of song position for multiple songs

---
+++ bootstrap-3.3.7-dist/js/lin.js
@@ -0,0 +1,66 @@
+
+var currentlyPlayingAudio = new Audio();
+var currentTitle;
+var arrayOfSongsToTimeStamp = [];
+var timeStampOne;
+var timeStampTwo;
+
+
+function resetButton(){
+	var element = document.querySelector('[aria-label='+currentTitle+']');
+	element.setAttribute('class', 'glyphicon glyphicon-play');
+}
+//resets last players button back to normal
+
+function savePreviousSongInfo(currentTitle){
+	arrayOfSongsToTimeStamp[currentTitle] = timeStampTwo - timeStampOne;
+	console.log(currentTitle + " is the currently at " + arrayOfSongsToTimeStamp[currentTitle]);
+	printDictionary();
+}
+
+function printDictionary(){
+	for(var key in arrayOfSongsToTimeStamp){
+		var val = arrayOfSongsToTimeStamp[key];
+		console.log("Song: "+key+" is at "+val);
+	}
+}
+
+
+function playSong(songTitle){
+	timeStampTwo = event.timeStamp;
+	savePreviousSongInfo(currentTitle);
+	var element = document.querySelector('[aria-label='+songTitle+']');
+	// get HTML element
+	var attributeValue = element.getAttribute("class"); 
+	// get value of class attribute
+	if(currentlyPlayingAudio.paused != true){
+		resetButton();
+	}
+	currentTitle = songTitle;
+	var audio = new Audio('music/'+songTitle+'.m4a');
+	currentlyPlayingAudio.pause();
+	timeStampOne = event.timeStamp;
+	if(attributeValue === "glyphicon glyphicon-play"){
+		element.setAttribute("class", "glyphicon glyphicon-pause");
+		if(songTitle in arrayOfSongsToTimeStamp){
+			audio.currentTime = arrayOfSongsToTimeStamp[songTitle];
+			audio.play();
+			currentlyPlayingAudio = audio;
+			// return;
+			console.log("Hello");
+		}
+		else{
+			audio.play();
+			currentlyPlayingAudio = audio;
+		}
+	}
+	else{
+		audio.pause();
+		element.setAttribute("class", "glyphicon glyphicon-play");
+	}
+
+}
+
+$('#song-box-header').click(function(e){
+	alert(e.pageX);
+})
