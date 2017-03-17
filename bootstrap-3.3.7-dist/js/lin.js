f3addcd61b6b2eac05d7c2f507dc205b3a7442a1
Lochoa11 <linochoa11690@gmail.com>
2017-03-17 11:41:53 -0400

    working on play buttons

--- bootstrap-3.3.7-dist/js/lin.js
+++ bootstrap-3.3.7-dist/js/lin.js
@@ -1,10 +1,6 @@
 
-var currentlyPlayingAudio = new Audio();
-var currentTitle;
-var arrayOfSongsToTimeStamp = [];
-var timeStampOne;
-var timeStampTwo;
-
+var currentAudio = new Audio();
+var currentElement;
 
 function resetButton(){
 	var element = document.querySelector('[aria-label='+currentTitle+']');
@@ -26,60 +22,25 @@ function printDictionary(){
 }
 
 
-// function playSong(songTitle){
-// 	// timeStampTwo = event.timeStamp;
-// 	var element = document.querySelector('[aria-label='+songTitle+']');
-// 	// get HTML element
-// 	var attributeValue = element.getAttribute("class"); 
-// 	// get value of class attribute
-// 	if(currentlyPlayingAudio.paused != true){
-// 		resetButton();
-// 	}
-// 	currentTitle = songTitle;
-// 	var audio = new Audio('music/'+songTitle+'.m4a');
-// 	currentlyPlayingAudio.pause();
-// 	timeStampOne = event.timeStamp;
-// 	if(attributeValue === "glyphicon glyphicon-play"){
-// 		element.setAttribute("class", "glyphicon glyphicon-pause");
-// 		if(songTitle in arrayOfSongsToTimeStamp){
-// 			audio.currentTime = arrayOfSongsToTimeStamp[songTitle];
-// 			audio.play();
-// 			currentlyPlayingAudio = audio;
-// 			// return;
-// 			console.log("Hello");
-// 		}
-// 		else{
-// 			audio.play();
-// 			currentlyPlayingAudio = audio;
-// 		}
-// 	}
-// 	else{
-// 		audio.pause();
-// 		element.setAttribute("class", "glyphicon glyphicon-play");
-// 	}
-// }
 
 function playSong(songTitle){
 	//song object
-	var audio = new Audio('music/'+ songTitle +'.m4a');
+	var audio = new Audio("music/"+ songTitle +".m4a");
 	//HTML element object
-	var element = document.querySelector('[aria-label='+ songTitle + ']');
+	var element = document.querySelector("[aria-label="+ songTitle + "]");
 	//get value of class attribute
-	var attributeValue = element.getAttribute("class");
-
-	if (attributeValue === 'glyphicon glyphicon-play') {
+	currentElement = element;
+	var attributeValue = currentElement.getAttribute("class");
+	if ((attributeValue === "glyphicon glyphicon-play") || (currentAudio.paused)) {
+		currentAudio.pause();
+		currentElement.setAttribute("class", "glyphicon glyphicon-pause");
 		audio.play();
-		attributeValue.setAttribute("class", "glyphicon glyphicon-pause");
+		currentAudio = audio;
+		currentElement = element;
 	}else{
-		audio.pause();
+		currentAudio.pause();
+		currentElement.setAttribute("class", "glyphicon glyphicon-play");
 	}
-		
 
 }
 
-
-
-//this alerts you where in the xdirection you are clicking
-// $('#song-box-header').click(function(e){
-// 	alert(e.pageX);
-// })
